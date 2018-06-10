5b1b679c4ff1d490db0b18776f100612353a9db3
Felix Zhou <felix990302@yahoo.ca>
2018-06-10 19:45:32 -0400

    able to auto navigate and accept a quest from doomwood

--- aqwbot/farmNecro.py
+++ aqwbot/farmNecro.py
@@ -1,12 +1,26 @@
 from pyimagesearch import imagesearch_loop, click_image
-import time
 import pyautogui
 
 # search for chat-bar to navigate
 chatpos = imagesearch_loop("aqwChatBar.png", 1, prefix="img/")
-print(chatpos)
 
-# navigate to lightguard
+# navigate to doomwood
 click_image("aqwChatBar.png", chatpos, "left", 0.2, prefix="img/", offset=5)
-pyautogui.typewrite(r'/join lightguard', interval=0.1)
+pyautogui.typewrite(r'/join doomwood', interval=0.1)
 pyautogui.typewrite(['enter'])
+
+# click on questmark
+questpos = imagesearch_loop("aqwDoomWoodQuestMark.png", 1, prefix="img/")
+click_image("aqwDoomWoodQuestMark.png", questpos, "left", 0.2, prefix="img/", offset=0.1)
+
+# click on quest button
+questbutton = imagesearch_loop("aqwQuestButton.png", 1, prefix="img/")
+click_image("aqwQuestButton.png", questbutton, "left", 0.2, prefix="img/", offset=5)
+
+# click on bony battalion
+bonybattalion = imagesearch_loop("aqwBonyBattalionLabel.png", 1, prefix="img/")
+click_image("aqwBonyBattalionLabel.png", bonybattalion, "left", 0.2, prefix="img/", offset=5)
+
+# click on accept button
+acceptbutton = imagesearch_loop("aqwAcceptButton.png", 1, prefix="img/")
+click_image("aqwAcceptButton.png", acceptbutton, "left", 0.2, prefix="img/", offset=5)
